212b049b93204b5c5682cb25e4d5d598dbc7360f
Ryan Chandler <ryangjchandler@gmail.com>
2022-04-08 20:57:26 +0100

    chore: cruft

--- config/slug.php
+++
@@ -1,5 +0,0 @@
-<?php
-// config for RyanChandler/LaravelSlug
-return [
-
-];
--- src/LaravelSlugServiceProvider.php
+++ src/LaravelSlugServiceProvider.php
@@ -4,22 +4,11 @@ namespace RyanChandler\Slug;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use RyanChandler\Slug\Commands\LaravelSlugCommand;
 
 class LaravelSlugServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('laravel-slug')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-slug_table')
-            ->hasCommand(LaravelSlugCommand::class);
+        $package->name('laravel-slug');
     }
 }
